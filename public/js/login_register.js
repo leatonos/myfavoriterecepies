ea1fc7f5102edf473f3b4d2bb37220eb84d3f20c
Pedrofmattosb <pedrofmattosb@gmail.com>
2021-08-12 19:20:08 -0300

    Changed google login position

--- public/js/login_register.js
+++ public/js/login_register.js
@@ -29,13 +29,43 @@ $(document).ready(function(){
     const auth = firebase.auth();
 
     const googleProvider = new firebase.auth.GoogleAuthProvider();
+    const EmailProvider = new firebase.auth.
 
-
-    //Shows Register form
+    //Login with google
     $("#loginGoogle").on("click",function(){
-
         auth.signInWithPopup(googleProvider);
-
     });
+
+
+    //Creating account with email
+    
+    /*
+    
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+  });
+    
+    */
+
+  //Login in with email
+/*
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+ */
 
 });
